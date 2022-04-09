66f76570fc08dabed294b49964482e5e7497232c
Gustavo400 <Gustavo4@live.unc.edu>
2022-04-09 15:39:56 -0400

    getting the lips.js to work

--- flips.js
+++ flips.js
@@ -1,6 +1,11 @@
 // Import the coinFlip function from your coin.mjs file
 import * as coin from "./modules/coin.mjs";
 
-// let variable = coin.coinFlips(7);
+let args = process.argv;
+
+let lastArg = process.argv[process.argv.length - 1];
+console.log(lastArg);
+
+// let variable = coin.coinFlips(  );
 // console.log(variable);
 // console.log(coin.countFlips(variable));
