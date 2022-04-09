7a07f1ff35b841ce16f94df6241358ab92780e80
Gustavo400 <Gustavo4@live.unc.edu>
2022-04-09 14:57:20 -0400

    Trying to export this that and the other idrk

--- flip.js
+++ flip.js
@@ -1,3 +1,9 @@
 // Import the coinFlip function from your coin.mjs file
 
+import * as coin from "/modules/coin.mjs";
+
 // Call the coinFlip function and put the return into STDOUT
+
+coin.coinFlip();
+
+// console.log("We don't know how to import");
