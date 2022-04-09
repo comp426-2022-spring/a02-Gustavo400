0ef77292aebc0681bab8eebb8038a227dc41ad47
Gustavo400 <Gustavo4@live.unc.edu>
2022-04-09 15:50:17 -0400

    this time actually finished flips for null values

--- flips.js
+++ flips.js
@@ -3,7 +3,8 @@ import * as coin from "./modules/coin.mjs";
 
 let lastArg = process.argv[process.argv.length - 1];
 let number = parseInt(lastArg.substr(9)); //9 is the length of "--number=" which is being taken out to find the actual number
-let variable = coin.coinFlips(number);
+let dracula = number ? number : 1;
+let results = coin.coinFlips(dracula);
 
-console.log(variable);
-console.log(coin.countFlips(variable));
+console.log(results);
+console.log(coin.countFlips(results));
