36015afe8c111f1a0eedc91d763da2b31aa66f7c
Gustavo400 <Gustavo4@live.unc.edu>
2022-04-09 16:07:15 -0400

    I think I got everything to work

--- guess-flip.js
+++ guess-flip.js
@@ -1,4 +1,13 @@
 // Import the coinFlip function from your coin.mjs file
 import * as coin from "./modules/coin.mjs";
 
-// console.log(coin.flipACoin("heads"));
+let whatWillItBe = process.argv[process.argv.length - 1];
+let guess = whatWillItBe.substr(7); //7 is the length of "--call=" which is being taken out to find the actual guess
+
+if (guess != "heads" && guess != "tails") {
+    console.log("Error: no input.");
+} else {
+    let outcome = coin.coinFlip();
+    let victory = guess === outcome ? "win" : "lose";
+    console.log(`{ call: '${guess}', flip: '${outcome}', result: '${victory}' }`);
+}
